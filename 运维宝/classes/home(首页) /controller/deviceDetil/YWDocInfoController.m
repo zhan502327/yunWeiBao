4c96a3c34f78c5e18904addd14e21953e636592a
zhan502327 <zhan502327@163.com>
2017-12-17 17:37:07 +0800

    no message

--- 运维宝/classes/home(首页) /controller/deviceDetil/YWDocInfoController.m
+++ 运维宝/classes/home(首页) /controller/deviceDetil/YWDocInfoController.m
@@ -14,10 +14,16 @@
 #import "ZYDownloadProgressView.h"
 #import "HWProgressView.h"
 
-@interface YWDocInfoController ()<UIDocumentInteractionControllerDelegate>
+#import <QuickLook/QuickLook.h>
+
+#import "TTOpenInAppActivity.h"
+
+@interface YWDocInfoController ()<UIDocumentInteractionControllerDelegate, QLPreviewControllerDataSource, UIWebViewDelegate>
 {
     //ZYDownloadProgressView *_progressView;
     CGFloat _progress;
+    
+    NSString *_filePath;
 }
 
 /** 最新主播列表 */
@@ -36,6 +42,8 @@
 @property (weak, nonatomic) UIView *cover;
 /** 进度条 */
 @property (nonatomic, weak) HWProgressView *progressView;
+
+@property (nonatomic, strong) UIDocumentInteractionController *doc;
 
 @end
 
@@ -101,7 +109,7 @@
     params[@"account_id"] = kGetData(@"account_id");
     params[@"a_id"] = self.a_id;    //请求数据
     [HMHttpTool post:url params:params success:^(id responseObj) {
-         
+        
         YWLog(@"getDeviceDoc--%@",responseObj);
         NSDictionary *deviceDocsDict = responseObj[@"data"];
         NSString *status = responseObj[@"code"];
@@ -117,10 +125,10 @@
             NSMutableArray *drawArr = [[NSMutableArray alloc] init];
             
             NSMutableArray *assetArr = [[NSMutableArray alloc] init];
-
+            
             for (YWDeviceDocs *deviceDocs in docGroup.DocPublic) {
                 [docsArr addObject:deviceDocs];
-               
+                
             }
             for (YWDeviceDocs *deviceDocs in docGroup.drawing) {
                 
@@ -140,7 +148,7 @@
             /**停止刷新*/
             [self.tableView.mj_header endRefreshing];
             [self.tableView.mj_footer endRefreshing];
-
+            
         }
         
         
@@ -170,7 +178,7 @@
         return self.docInfos.count;
     }else {
         return self.assetInfos.count;
-
+        
     }
 }
 
@@ -183,7 +191,7 @@
     if (indexPath.section == 0 && self.docInfos.count>0) {
         
         cell.deviceDoc = self.drawInfos[indexPath.row];;
-
+        
     } else if (indexPath.section == 1){
         
         cell.deviceDoc = self.docInfos[indexPath.row];
@@ -217,26 +225,50 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    
     if (indexPath.section == 0 && self.docInfos.count>0) {
-        
-        YWDeviceDocs *deviceModel = self.drawInfos[indexPath.row];;
-        
+        //        YWDeviceDocs *deviceModel = self.drawInfos[indexPath.row];;
     } else if (indexPath.section == 1){
-        
         YWDeviceDocs *deviceModel = self.docInfos[indexPath.row];
-    }else if (indexPath.section == 2 && self.docInfos.count>0){
         
+        
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        
+        NSString *documentsDirectory = [paths lastObject];
+        NSLog(@"app_home_doc: %@",documentsDirectory);
+        
+        //        NSFileManager *fileManager = [NSFileManager defaultManager];
+        
+        //        NSString *filePath = [documentsDirectory stringByAppendingPathComponent:docPath]; //docPath为文件名
+        
+        
+        NSString *fileName = [NSString stringWithFormat:@"?token=%@&account_id=%@&file_id=%@&file_path=%@",kGetData(@"token"), kGetData(@"account_id"), deviceModel.file_id, deviceModel.file_path];
+        
+        [self downloadDocxWithDocPath:documentsDirectory fileName:fileName ];
+        
+        
+        
+    }else if (indexPath.section == 2 && self.docInfos.count>0){
         YWDeviceDocs *deviceModel = self.assetInfos[indexPath.row];;
-    }
+        
+        
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString *documentsDirectory = [paths lastObject];
 
-    
-    
-    
-    [self tableViewClick];
+        NSLog(@"app_home_doc: %@",documentsDirectory);
+        
+        //        NSFileManager *fileManager = [NSFileManager defaultManager];
+        
+        //        NSString *filePath = [documentsDirectory stringByAppendingPathComponent:docPath]; //docPath为文件名
+        
+        
+        NSString *fileName = [NSString stringWithFormat:@"?token=%@&account_id=%@&file_id=%@&file_path=%@",kGetData(@"token"), kGetData(@"account_id"), deviceModel.file_id, deviceModel.file_path];
+        
+        [self downloadDocxWithDocPath:documentsDirectory fileName:fileName];
+        
+    }
+    //    [self tableViewClick];
     //添加定时器
-    [self addTimer];
-    
+    //    [self addTimer];
 }
 
 
@@ -249,12 +281,12 @@
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     
     NSString *documentsDirectory = [paths objectAtIndex:0];
-//    NSString *documentsDirectory = [paths lastObject];
-
+    //    NSString *documentsDirectory = [paths lastObject];
+    
     
     NSString *filePath = [documentsDirectory stringByAppendingPathComponent:docPath];
-//    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:docPath]; //docPath为文件名
-
+    //    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:docPath]; //docPath为文件名
+    
     NSFileManager *fileManager = [NSFileManager defaultManager];
     
     BOOL result = [fileManager fileExistsAtPath:filePath];
@@ -271,7 +303,6 @@
             [self openDocxWithPath:filePath];
         }]];
         
-//        [alertController.actions setValue:[UIColor colorWithHexString:@"3998ef"] forKey:@"_titleTextColor"];
         [self presentViewController:alertController animated:YES completion:nil];
         
     }else {
@@ -285,13 +316,12 @@
             [self downloadDocxWithDocPath:documentsDirectory fileName:docPath];
         }]];
         
-//        [alertController.actions setValue:[UIColor colorWithHexString:@"3998ef"] forKey:@"_titleTextColor"];
         [self presentViewController:alertController animated:YES completion:nil];
         
     }
-
+    
     return result;
-
+    
 }
 
 
@@ -303,7 +333,10 @@
  */
 -(void)downloadDocxWithDocPath:(NSString *)docPath fileName:(NSString *)fileName {
     [MBProgressHUD showMessage:@"正在下载文件" toView:self.view];
-    NSString *urlString = @"http://66.6.66.111:8888/UploadFile/";
+    
+    NSString *urlStr = @"/assets_doucment_down.php";
+    NSString *urlString = [YWBaseURL stringByAppendingFormat:@"%@",urlStr];
+    
     
     urlString = [urlString stringByAppendingString:fileName];
     
@@ -315,17 +348,22 @@
     NSURLSessionDownloadTask *task = [manager downloadTaskWithRequest:request progress:&progress destination:^NSURL * _Nonnull(NSURL * _Nonnull targetPath, NSURLResponse * _Nonnull response) {
         NSString *path = [docPath stringByAppendingPathComponent:fileName];
         NSLog(@"文件路径＝＝＝%@",path);
+    
         return [NSURL fileURLWithPath:path];//这里返回的是文件下载到哪里的路径 要注意的是必须是携带协议file://
+        
+        
+        
     } completionHandler:^(NSURLResponse * _Nonnull response, NSURL * _Nullable filePath, NSError * _Nullable error) {
         [MBProgressHUD hideHUDForView:self.view];
         [MBProgressHUD showSuccess:@"下载完成,正在打开" toView:self.view];
-        //        if (error) {
-        //
-        //        }else {
-        NSString *name = [filePath path];
-        NSLog(@"下载完成文件路径＝＝＝%@",name);
-        [self openDocxWithPath:name];//打开文件
-        //        }
+        if (error) {
+            NSLog(@"%@",error);
+            [SVProgressHUD showErrorWithStatus:@"下载失败，请重新下载"];
+        }else {
+            NSString *name = [filePath path];
+            NSLog(@"下载完成文件路径＝＝＝%@",name);
+            [self openDocxWithPath:name];//打开文件
+        }
     }];
     [task resume];//开始下载 要不然不会进行下载的
     
@@ -338,10 +376,96 @@
  */
 -(void)openDocxWithPath:(NSString *)filePath {
     
-    UIDocumentInteractionController *doc= [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:filePath]];
-    doc.delegate = self;
-    [doc presentPreviewAnimated:YES];
+//        _filePath = @"/Users/zhangshuai/Library/Developer/CoreSimulator/Devices/07D45D6F-0009-4FDA-BF06-D56BACC3DE15/data/Containers/Data/Application/5BF98349-F324-40DD-9820-6D7A33C14024/Documents/11111";
+    
+    self.doc= [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:filePath]];
+    self.doc.delegate = self;
+    [self.doc presentPreviewAnimated:YES];
+//    [doc presentOpenInMenuFromRect:CGRectMake(10, 100, SCREEN_WIDTH - 20, SCREEN_HEIGHT - 200) inView:self.view animated:YES];
+//    [doc presentOpenInMenuFromRect:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT) inView:self.view animated:YES];
+//    [self.doc presentOptionsMenuFromRect:self.view.bounds inView:self.view animated:YES];
+
+    
+    
+//    NSURL *url = [NSURL fileURLWithPath:@"/Users/zhangshuai/Library/Developer/CoreSimulator/Devices/07D45D6F-0009-4FDA-BF06-D56BACC3DE15/data/Containers/Data/Application/3ADD2BCF-9145-4FF5-A863-C911B35912CD/Documents/\?token\=tm2tcn60n4eaahhfelj30fjvr1\&account_id\=10005\&file_id\=10112\&file_path\=\(null\) "];
+//
+//    UIWebView *webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+//    [webView loadRequest:[NSURLRequest requestWithURL:url]];
+    
+//    /Users/zhangshuai/Library/Developer/CoreSimulator/Devices/07D45D6F-0009-4FDA-BF06-D56BACC3DE15/data/Containers/Data/Application/5BF98349-F324-40DD-9820-6D7A33C14024/Documents/?token=tm2tcn60n4eaahhfelj30fjvr1&account_id=10005&file_id=10005&file_path=(null)
+    
+//
+
+//    _filePath = filePath;
+//    QLPreviewController *previewVC = [[QLPreviewController alloc] init];
+//    previewVC.dataSource = self;
+//    [self presentViewController:previewVC animated:YES completion:nil];
+    
+    
+//    [self loadWebVIew];
+
+//    [self loadPartWithFileParh:filePath];
+    
+//    [self loadDocument:filePath];
+    
 }
+
+///////       第四步
+
+//已经下载了的文件用webview显示
+
+-(void)loadDocument:(NSString *)documentName
+
+{
+    
+    UIWebView *webView = [[UIWebView alloc]initWithFrame:CGRectMake(0, 64, SCREEN_WIDTH, 200)];
+    webView.backgroundColor = [UIColor redColor];
+    webView.delegate = self;
+    webView.scalesPageToFit = YES;
+    [self.view addSubview:webView];
+    [self.view bringSubviewToFront:webView];
+    
+    NSURL *url = [NSURL fileURLWithPath:documentName];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    [webView loadRequest:request];
+    
+    
+    
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView{
+    
+    NSLog(@"webViewDidFinishLoad");
+    
+    
+}
+
+
+
+
+- (void)loadPartWithFileParh:(NSString *)filePath{
+    NSURL *URL = [NSURL fileURLWithPath:filePath];
+
+    TTOpenInAppActivity *openInAppActivity = [[TTOpenInAppActivity alloc] initWithView:self.view andRect:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+    UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems:@[URL] applicationActivities:@[openInAppActivity]];
+    
+    if(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone){
+        // Store reference to superview (UIActionSheet) to allow dismissal
+        openInAppActivity.superViewController = activityViewController;
+        // Show UIActivityViewController
+        [self presentViewController:activityViewController animated:YES completion:NULL];
+    } else {
+        // Create pop up
+//        self.activityPopoverController = [[UIPopoverController alloc] initWithContentViewController:activityViewController];
+//        // Store reference to superview (UIPopoverController) to allow dismissal
+//        openInAppActivity.superViewController = self.activityPopoverController;
+//        // Show UIActivityViewController in popup
+//        [self.activityPopoverController presentPopoverFromRect:((UIButton *)sender).frame inView:self.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+    }
+}
+
 #pragma mark - UIDocumentInteractionControllerDelegate
 //必须实现的代理方法 预览窗口以模式窗口的形式显示，因此需要在该方法中返回一个view controller ，作为预览窗口的父窗口。如果你不实现该方法，或者在该方法中返回 nil，或者你返回的 view controller 无法呈现模式窗口，则该预览窗口不会显示。
 
@@ -357,8 +481,92 @@
 
 - (CGRect)documentInteractionControllerRectForPreview:(UIDocumentInteractionController*)controller {
     
-    return CGRectMake(0, 30, SCREEN_WIDTH , SCREEN_HEIGHT);
+    return CGRectMake(0, 200, SCREEN_WIDTH , SCREEN_HEIGHT);
 }
+
+
+
+
+
+- (void)loadWebVIew{
+    
+    UIWebView *_webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height - 64)];
+    _webView.delegate = self;
+    _webView.backgroundColor = [UIColor redColor];
+    _webView.scalesPageToFit = YES;
+    [self.view addSubview:_webView];
+    
+    
+    //获取文件路径
+    NSFileManager *manager = [NSFileManager defaultManager];
+    NSArray *array = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    //获取document文件夹路径
+    NSString *documents = [array lastObject];
+    //拼接绝对路径
+    NSString *documentPath = [documents stringByAppendingPathComponent:@"myReport/file"];
+    //存数据的具体文件夹
+    // [manager createDirectoryAtPath:documentPath withIntermediateDirectories:YES attributes:nil error:nil];
+    //得到文件名
+    NSArray *fileNameArray = [manager subpathsAtPath:documentPath];
+    
+    NSString *path = [NSString stringWithFormat:@"%@/%@", documentPath, fileNameArray[0]];
+    
+    //加载文件
+    [self loadDocument: path inView:_webView];
+    
+}
+#pragma mark -=-#pmark - 加载文件
+- (void)loadDocument:(NSString *)documentPath inView:(UIWebView *)webView{
+    // NSString *path = [[NSBundle mainBundle] pathForResource:documentName ofType:nil];
+    
+            _filePath = @"/Users/zhangshuai/Library/Developer/CoreSimulator/Devices/07D45D6F-0009-4FDA-BF06-D56BACC3DE15/data/Containers/Data/Application/5BF98349-F324-40DD-9820-6D7A33C14024/Documents/11111";
+
+    
+    NSURL *url = [NSURL fileURLWithPath:_filePath];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [webView loadRequest:request];
+}
+
+
+
+
+
+
+
+
+
+//实现代理协议
+#pragma mark-----------QLPreviewControllerDataSource
+
+//要显示的文件的数量
+/*!
+ * @abstract Returns the number of items that the preview controller should preview.
+ * @param controller The Preview Controller.
+ * @result The number of items.
+ */
+- (NSInteger)numberOfPreviewItemsInPreviewController:(QLPreviewController *)controller{
+    return 1;
+}
+
+
+- (id <QLPreviewItem>)previewController:(QLPreviewController *)controller previewItemAtIndex:(NSInteger)index{
+    //这个是加载的本地的pdf的文件，doc的同理
+    NSURL *url = [NSURL fileURLWithPath:_filePath];
+    return url;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -427,7 +635,7 @@
     [_timer invalidate];
     [self.progressView removeFromSuperview];
     [self.cover removeFromSuperview];
-
+    
     _timer = nil;
 }
 
